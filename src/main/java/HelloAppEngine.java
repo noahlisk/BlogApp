9ec28c6356b08f5726e0ba179e043221eb67ea05
Noah <noahlisk@utexas.edu>
2020-02-18 19:36:01 -0600

    Testing commit

--- src/main/java/HelloAppEngine.java
+++ src/main/java/HelloAppEngine.java
@@ -17,8 +17,7 @@ public class HelloAppEngine extends HttpServlet {
 
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
-
+    
     response.getWriter().print("Hello App Engine!\r\n");
-
   }
 }
